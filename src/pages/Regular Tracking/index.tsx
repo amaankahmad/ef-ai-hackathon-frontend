0852b9cc2fd6c102ca5632357c7f1f59cf66b830
amaankahmad <amaankahmad@gmail.com>
2023-09-08 03:35:46 -0700

    regular tracking

--- src/pages/Regular Tracking/index.tsx
+++ src/pages/Regular Tracking/index.tsx
@@ -1,28 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-import { useNavigate } from "react-router-dom";
+// Import necessary modules and hooks
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
 import RootPage from "../root";
+import { Button } from "flowbite-react";
 
+// Define the ConfirmationPage component
 const RegularTrackingPage = () => {
+  // Use React Router's hooks to get location and navigation functions
   const navigate = useNavigate();
 
+  // Get date and time from location state or use default values
+  const date = "Friday, September 15th";
+  const time = "8:00 AM";
+
+  // State and ref for handling image
+  const [imageSrc, setImageSrc] = useState(null);
+  const fileInputRef = useRef(null);
+
+  // Placeholder function for adding event to calendar
+  const addToCalendar = () => {
+    alert("Add to Calendar feature not yet implemented.");
+  };
+
   return (
-    <RootPage header="error">
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="xs:text-5xl text-6xl md:text-8xl font-bold mb-6 flex items-center text-center">
-          still under development
+    <RootPage header="home">
+      <div className="flex flex-col items-center justify-center h-screen px-4">
+        <h1 className="text-4xl font-bold mb-12">
+          All seems good but let's keep track of any changes. See you in a week
         </h1>
-        <button
-          className="px-8 py-4 my-[4%] text-black rounded-full text-lg font-semibold"
-          style={{ backgroundColor: "#FFFFFF" }}
-          onClick={() => navigate("/")}
+        <div className="text-align-left" style={{ color: "grey" }}>
+          <div>
+            {/* Display appointment information in grey */}
+            Here is a calendar reminder for your next session.
+          </div>
+          <div>
+            {/* Display location information in grey */}
+            {date} at {time}.
+          </div>
+        </div>
+        {/* Add to calendar button */}
+        <Button
+          onClick={addToCalendar}
+          className="mt-12"
+          style={{ backgroundColor: "#3296BC" }}
         >
-          in the meantime, lets take you home
-        </button>
+          Add to Calendar
+        </Button>
       </div>
     </RootPage>
   );
 };
 
+// Export the ConfirmationPage component
 export default RegularTrackingPage;
